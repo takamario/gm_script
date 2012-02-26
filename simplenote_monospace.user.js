31036caff8c21e7e8cd9453139ee18df5c742565
Shoei Takamaru <shoei.takamaru@gmail.com>
2012-02-26 23:25:07 +0900

    added simplenote_monospace.user.js

---
+++ simplenote_monospace.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==  
+// @name        simplenote.user.js
+// @namespace   https://github.com/takamario/gm_script
+// @description Change font to monospace at Simplenote
+// @include     https://simple-note.appspot.com/
+// @excludes    
+// ==/UserScript==  
+(function(){document.getElementById('txtarea').style.fontFamily = 'monospace';})();
